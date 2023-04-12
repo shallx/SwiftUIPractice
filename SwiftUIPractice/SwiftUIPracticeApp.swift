7cc5e56ac0f920386439d797c313196289513210
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2023-04-12 12:55:05 +0600

    Initial Commit

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Macuser on 12/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
